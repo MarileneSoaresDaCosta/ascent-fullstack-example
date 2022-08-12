9a296edc359497c907f052a319f87b07671aeac5
Marilene Soares da Costa <lene626@gmail.com>
2022-08-12 11:27:21 -0600

    Initial commit

---
+++ src/main/java/com/galvanize/fullstackexample/FullstackExampleApplication.java
@@ -0,0 +1,13 @@
+package com.galvanize.fullstackexample;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FullstackExampleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FullstackExampleApplication.class, args);
+	}
+
+}
